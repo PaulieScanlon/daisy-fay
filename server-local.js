4eda94997169a04562cfe0971dd7f5c6e9de6f3d
Paul Scanlon <pauliescanlon@gmail.com>
2023-10-25 15:31:04 -0400

    test: define createServer as async function

--- server-local.js
+++ server-local.js
@@ -7,43 +7,47 @@ import resolve from './src/utils/resolve.js';
 
 const app = express();
 
-let template, render, serverFile, serverFunction, serverData;
+const createServer = async () => {
+  let template, render, serverFile, serverFunction, serverData;
 
-const vite = await createViteServer({
-  server: {
-    middlewareMode: true,
-  },
-  appType: 'custom',
-});
+  const vite = await createViteServer({
+    server: {
+      middlewareMode: true,
+    },
+    appType: 'custom',
+  });
 
-app.use(vite.middlewares);
+  app.use(vite.middlewares);
 
-app.use('*', async (req, res) => {
-  const url = req.originalUrl;
-  const safeUrl = url === '/' ? '/index' : req.originalUrl;
+  app.use('*', async (req, res) => {
+    const url = req.originalUrl;
+    const safeUrl = url === '/' ? '/index' : req.originalUrl;
 
-  try {
-    template = fs.readFileSync(resolve('index.html', import.meta.url), 'utf-8');
-    template = await vite.transformIndexHtml(url, template);
-    render = (await vite.ssrLoadModule('/src/entry-server.jsx')).render;
-    serverFile = `src/pages${safeUrl}/function.js`;
+    try {
+      template = fs.readFileSync(resolve('index.html', import.meta.url), 'utf-8');
+      template = await vite.transformIndexHtml(url, template);
+      render = (await vite.ssrLoadModule('/src/entry-server.jsx')).render;
+      serverFile = `src/pages${safeUrl}/function.js`;
 
-    if (exists(serverFile)) {
-      serverFunction = (await vite.ssrLoadModule(serverFile)).GET;
-      serverData = await serverFunction();
+      if (exists(serverFile)) {
+        serverFunction = (await vite.ssrLoadModule(serverFile)).GET;
+        serverData = await serverFunction();
+      }
+
+      const dom = render(url, serverData);
+      const script = `<script>window.__data__=${JSON.stringify(serverData)}</script>`;
+      const html = template.replace(`<!--ssr-outlet-->`, `${dom} ${script}`);
+
+      res.status(200).set({ 'Content-Type': 'text/html' }).end(html);
+    } catch (event) {
+      vite.ssrFixStacktrace(event);
+      console.log(event.stack);
+      res.status(500).end(event.stack);
     }
+  });
+};
 
-    const dom = render(url, serverData);
-    const script = `<script>window.__data__=${JSON.stringify(serverData)}</script>`;
-    const html = template.replace(`<!--ssr-outlet-->`, `${dom} ${script}`);
-
-    res.status(200).set({ 'Content-Type': 'text/html' }).end(html);
-  } catch (event) {
-    vite.ssrFixStacktrace(event);
-    console.log(event.stack);
-    res.status(500).end(event.stack);
-  }
-});
+createServer();
 
 app.listen(4173, () => {
   console.log('http://localhost:4173');
--- server.js
+++ server.js
@@ -6,40 +6,44 @@ import resolve from './src/utils/resolve.js';
 
 const app = express();
 
-let template, render, serverFile, serverFunction, serverData;
+const createServer = async () => {
+  let template, render, serverFile, serverFunction, serverData;
 
-app.use((await import('compression')).default());
-app.use(
-  (await import('serve-static')).default(resolve('dist/client', import.meta.url), {
-    index: false,
-  })
-);
+  app.use((await import('compression')).default());
+  app.use(
+    (await import('serve-static')).default(resolve('dist/client', import.meta.url), {
+      index: false,
+    })
+  );
 
-app.use('*', async (req, res) => {
-  const url = req.originalUrl;
-  const safeUrl = url === '/' ? '/index' : req.originalUrl;
+  app.use('*', async (req, res) => {
+    const url = req.originalUrl;
+    const safeUrl = url === '/' ? '/index' : req.originalUrl;
 
-  try {
-    template = fs.readFileSync(resolve('./dist/client/index.html', import.meta.url), 'utf-8');
-    render = (await import('./dist/server/entry-server.js')).render;
-    serverFile = `./dist/functions${safeUrl}/function.js`;
+    try {
+      template = fs.readFileSync(resolve('./dist/client/index.html', import.meta.url), 'utf-8');
+      render = (await import('./dist/server/entry-server.js')).render;
+      serverFile = `./dist/functions${safeUrl}/function.js`;
 
-    if (exists(serverFile)) {
-      serverFunction = (await import(serverFile)).GET;
-      serverData = await serverFunction();
+      if (exists(serverFile)) {
+        serverFunction = (await import(serverFile)).GET;
+        serverData = await serverFunction();
+      }
+
+      const dom = render(url, serverData);
+      const script = `<script>window.__data__=${JSON.stringify(serverData)}</script>`;
+
+      const html = template.replace(`<!--ssr-outlet-->`, `${dom} ${script}`);
+
+      res.status(200).set({ 'Content-Type': 'text/html' }).end(html);
+    } catch (event) {
+      console.log(event.stack);
+      res.status(500).end(event.stack);
     }
+  });
+};
 
-    const dom = render(url, serverData);
-    const script = `<script>window.__data__=${JSON.stringify(serverData)}</script>`;
-
-    const html = template.replace(`<!--ssr-outlet-->`, `${dom} ${script}`);
-
-    res.status(200).set({ 'Content-Type': 'text/html' }).end(html);
-  } catch (event) {
-    console.log(event.stack);
-    res.status(500).end(event.stack);
-  }
-});
+createServer();
 
 export default app;
 
