f1e25a9f57e793f8f7b8936db2b703c581447bf7
Paul Scanlon <pauliescanlon@gmail.com>
2023-11-02 18:31:58 -0400

    refactor/clean-up  (#3)
    * refactor: change a few things around
    
    * feat: update favico

--- src/pages/page-2/function.js
+++ src/pages/page-2/function.js
@@ -1,4 +1,4 @@
-export const GET = async () => {
+export const getServerData = async () => {
   const request = await fetch('https://dummyjson.com/carts?limit=10');
   const data = await request.json();
 
